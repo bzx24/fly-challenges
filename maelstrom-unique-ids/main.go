06431223f417e07c41cd8f3a5b45b8b846a57ea2
Brian Xu <brianxu16@gmail.com>
2026-01-10 20:06:34 -0500

    unique ids

---
+++ maelstrom-unique-ids/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	uuid "github.com/google/uuid"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	node := maelstrom.NewNode()
+
+	node.Handle("generate", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["id"] = uuid.New()
+		body["type"] = "generate_ok"
+
+		return node.Reply(msg, body)
+	})
+
+	if err := node.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
